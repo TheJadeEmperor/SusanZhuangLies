1fb730ce1a4b68b94067a48ab4d11d090f2d8f46
TheJadeEmperor <TheJadeEmperor@users.noreply.github.com>
2024-01-19 18:28:39 -0500

    first draft

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+$seoUrl = 'https://susanzhuanglies.com/';
+$seoSiteName = 'Scammer Susan Zhuang for City Council District 43';
+$seoDesc = "Scammer Susan Zhuang's campaign is full of scams, including the biggest scam, which is the fact that she is funded by the CCP - the Chinese Communist Party. She and her supporter Communist Colton has tricked both Chinese and American citizens into thinking she's looking out for their best interests.";
+ 
 
 include('Ron DeSantis’ World of Lies.html');
 ?>
