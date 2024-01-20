f9c85f270d9ce080f5c18be3de02441d680186e1
TheJadeEmperor <TheJadeEmperor@users.noreply.github.com>
2024-01-20 18:13:05 -0500

    add Chinese language

--- index.php
+++ index.php
@@ -4,5 +4,14 @@ $seoSiteName = 'Scammer Susan Zhuang for City Council District 43';
 $seoDesc = "Scammer Susan Zhuang's campaign is full of scams, including the biggest scam, which is the fact that she is funded by the CCP - the Chinese Communist Party. She and her supporter Communist Colton has tricked both Chinese and American citizens into thinking she's looking out for their best interests.";
 $shareImg = $seoUrl.'images/scam_susan.png';
 
-include('Ron DeSantis’ World of Lies.html');
+global $CN;
+
+if($_GET['la'] == 'CN') {
+    $la = 'CN';
+}
+else {
+    $la = 'EN';
+}
+
+include('index.html');
 ?>
